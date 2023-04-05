d04255b320f8bfa2455cd730efadd281418ba929
Vaishak Kumar N P <vaishakmanoj96@gmail.com>
2023-04-05 11:10:09 +0530

    contact list

---
+++ src/components/AddContact.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class AddContact extends React.Component {
+    render() {
+        return(
+            <div className="ui main">
+                <h2>AddContact</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" placeholder="Name"/>
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" placeholder="Email"/>
+                    </div>
+                    <button className="ui button blue" >Add</button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default AddContact;
