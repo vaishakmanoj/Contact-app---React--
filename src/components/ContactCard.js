7d7dc09b90a38a0b6fe0e8d2fee6bd2d348dc7c7
Vaishak Kumar N P <vaishakmanoj96@gmail.com>
2023-04-05 15:13:47 +0530

    useEffect

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -9,10 +9,12 @@ const ContactCard = (props) => {
             <img className="ui avatar image" src="{user}" alt="user" />
         <div className="content">
            <div className="header">{name}</div>
-           <div>{email}</div>
+           <div>{email}</div> 
         </div>
         <i className="trash alternate outline icon" 
-        style={{color:"red", marginTop: "7px" }}></i>
+        style={{color:"red", marginTop: "7px" }}
+        onClick={() => props.clickHandler(id)}
+        ></i>
    </div>
 
     )
